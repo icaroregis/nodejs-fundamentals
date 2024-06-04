00e85e10f7b5db15aca9eca10add5524e7e9df82
icaroalmeida-somma <icaro.almeida@somma-it.com>
2024-06-03 22:03:18 -0300

    feature: Usando o conceito de buffers na criação de um novo usuário

--- src/server.js
+++ src/server.js
@@ -12,18 +12,31 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request;
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (error) {
+    request.body = null;
+  }
 
   if (method === 'GET' && url === '/users') {
     return response.setHeader('Content-type', 'application/json').end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
+    const { nome, email } = request.body;
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'john.doe@example.com',
+      nome,
+      email,
     });
 
     return response.writeHead(201).end();
