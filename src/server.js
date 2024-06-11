67446f79a8d0dc5d41f59b892efb6fd36738336b
icaroalmeida-somma <icaro.almeida@somma-it.com>
2024-06-10 21:58:53 -0300

    feature: create a new user and persist data

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
+import { Database } from './middlewares/database.js';
 
 // request => conseguimos acessar através da requisição que está chegando em nosso servidor todos os parâmetros da requisição de quem esta chamando o nosso servidor, exemplo: Criar um usuário(name, email, senha).
 
@@ -11,27 +12,29 @@ import { json } from './middlewares/json.js';
 
 // HTTP Status Code.
 
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
-  //a nossa requisição chegou aqui e foi interceptada por outro arquivo, isso é um middleware e geralmente são fácies de identificar pois recebem a request e response.
+  //a nossa requisição chegou aqui e foi interceptada por outro arquivo, isso é um middleware e geralmente são fáceis de identificar pois recebem a request e response.
   await json(request, response);
 
   if (method === 'GET' && url === '/users') {
+    const users = database.select('users');
     return response.setHeader('Content-type', 'application/json').end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
     const { nome, email } = request.body;
 
-    users.push({
+    const user = {
       id: 1,
       nome,
       email,
-    });
+    };
 
+    database.insert('users', user);
     return response.writeHead(201).end();
   }
 
